13da420028c70b8ec3adb23ac49b0d9c2897a842
hosh <r07922004@ntu.edu.tw>
2019-06-24 00:03:19 +0800

    finish

---
+++ source/corpus2invFile.py
@@ -0,0 +1,70 @@
+import json
+import math
+from argparse import ArgumentParser
+import pandas as pd
+from helper import *
+
+parser = ArgumentParser()
+parser.add_argument("-c", "--corpus_file", default="../data.json", dest="corpus_file", help="Pass in a .csv file.")
+args = parser.parse_args()
+
+# TF[i] : Contain the TF of all unigrams & bigrams in doc i
+TF = {}
+name_TF = {}
+# INV_FILE[i] : Contain the document list that has term i
+INV_FILE = {}
+name_INV_FILE = {}
+# DOC_LENGTH[i] : The length of doc i
+DOC_LENGTH = {}
+# IDF[i] : Contain the IDF of term i
+IDF = {}
+
+with open(args.corpus_file, encoding="utf-8") as f:
+    corpus = json.load(f)
+
+punctuation = set([
+    "，", "、", "　", "《", "〈", "＜", "<", ",", "。", "・", "·", "》", "〉", "＞", ">", ".",
+    "？", "/", "／", "÷", "?", "：", "；", ":", ";", "“", "”", "\"", "〃", "'",
+    "『", "「", "【", "〔", "［", "{", "[", "』", "」", "】", "〕", "］", "}", "]", "｜", "|", "\\",
+    "~", "!", "@", "#", "$#", "%", "^", "&", "*", "(", ")", "_", "__", "+", "="
+])
+
+for doc_idx, doc in enumerate(corpus['articles']):
+    #print("doc_id", doc_id)
+    doc_id = str(doc_idx)
+    for term in doc['article_tf']:
+        count = doc['article_tf'][term]
+        if term in punctuation: continue
+        if not isName(term): continue
+        
+        if term not in INV_FILE.keys():
+            INV_FILE[term] = []
+        INV_FILE[term].append(doc_id)
+
+        if doc_id not in TF.keys():
+            TF[doc_id] = {}
+        TF[doc_id][term] = count
+        if doc_id not in DOC_LENGTH.keys():
+            DOC_LENGTH[doc_id] = 0
+        DOC_LENGTH[doc_id] += count
+
+        if term not in IDF.keys():
+            IDF[term] = 0
+        IDF[term] += 1
+
+        #print(term)
+
+N = len(corpus['articles'])
+for term in IDF.keys():
+    k = IDF[term]
+    IDF[term] = math.log( (N - k + 0.5)/(k + 0.5) ) 
+
+with open("../TF.json", "w", encoding="utf-8") as f:
+    json.dump(TF, f, ensure_ascii=False)
+with open("../INV_FILE.json", "w", encoding="utf-8") as f:
+    json.dump(INV_FILE, f, ensure_ascii=False)
+with open("../DOC_LENGTH.json", "w", encoding="utf-8") as f:
+    json.dump(DOC_LENGTH, f, ensure_ascii=False)
+with open("../IDF.json", "w", encoding="utf-8") as f:
+    json.dump(IDF, f, ensure_ascii=False)
+
---
+++ source/test.py
@@ -0,0 +1,7 @@
+from helper import *
+print(last_names)
+print(isName("陳紅號"))
+
+doc = {"a": 1, "b": 1, "c": 1}
+for id in doc:
+    print(id)
